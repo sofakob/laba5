42df756cf4bf03912f9ae2350c85557f52236a9f
sofakob <hhitech628@gmail.com>
2024-05-24 20:49:58 +0300

    delete multyple

--- laba5Test.py
+++ laba5Test.py
@@ -1,6 +1,5 @@
 import unittest
 from laba5 import add 
-from laba5 import multiply
 class TestAddFunction(unittest.TestCase):
     
     def test_add_integers(self):
@@ -26,33 +25,6 @@ class TestAddFunction(unittest.TestCase):
         self.assertEqual(add(0, 0), 0)
         self.assertEqual(add(0, 5), 5)
         self.assertEqual(add(-5, 0), -5)
-class TestMultiplyFunction(unittest.TestCase):
-    
-    def test_multiply_integers(self):
-        """Тест множення цілих чисел."""
-        self.assertEqual(multiply(2, 3), 6)
-        self.assertEqual(multiply(-2, -3), 6)
-        self.assertEqual(multiply(-2, 3), -6)
-        self.assertEqual(multiply(0, 5), 0)
-    
-    def test_multiply_floats(self):
-        """Тест множення чисел з плаваючою комою."""
-        self.assertAlmostEqual(multiply(2.5, 3.5), 8.75)
-        self.assertAlmostEqual(multiply(-2.5, -3.5), 8.75)
-        self.assertAlmostEqual(multiply(-2.5, 3.5), -8.75)
-        self.assertAlmostEqual(multiply(0.0, 5.0), 0.0)
-
-    def test_multiply_integer_and_float(self):
-        """Тест множення цілого числа на число з плаваючою комою."""
-        self.assertAlmostEqual(multiply(2, 3.5), 7.0)
-        self.assertAlmostEqual(multiply(-2, 3.5), -7.0)
-        self.assertAlmostEqual(multiply(0, 3.5), 0.0)
-
-    def test_multiply_zero(self):
-        """Тест множення на нуль."""
-        self.assertEqual(multiply(0, 0), 0)
-        self.assertEqual(multiply(0, 5), 0)
-        self.assertEqual(multiply(5, 0), 0)
 
 
 if __name__ == '__main__':
